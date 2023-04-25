c1f3fbe68765334f829db5ea67b831bca66b9d70
Hitesh <hiteshkumar78785@gmail.com>
2023-04-25 22:04:13 +0530

    predict pipeline updated

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -22,16 +22,51 @@ class PredictPipeline:
             raise CustomException(e, sys)
 
 class CustomData:
-    def __init__(self):
+    def __init__(self,
+                 CarName : str,
+                    fueltype :str, aspiration :str,
+                    doornumber: str, carbody: str, drivewheel :str, enginelocation: str, wheelbase: float,
+                    carlength: float, carwidth: float, carheight:float, curbweight:float, enginetype:str,
+                    cylindernumber: str, enginesize: float, fuelsystem: str, boreratio:float, stroke: float,
+                    compressionratio: float, horsepower: float, peakrpm: float, citympgL: float, highwaympg: float):
         '''
         here is the all data that we recieve from the user or our web app
         '''    
+
     def get_data_as_data_frame(self):
         '''
         Here is the code for converting the user data to dataframe
         '''
-        
         try:
-            pass
+            custom_data_input_dict = {
+                'CarName' : [self.CarName],
+                'symboling': [self.symboling],
+                'CarName': [self.CarName],
+                'fueltype': [self.fueltype],
+                'aspiration' : [self.aspiration],
+                'doornumber' : [self.doornumber], 
+                'carbody' : [self.carbody],
+                'drivewheel': [self.drivewheel], 
+                'enginelocation': [self.enginelocation], 
+                'wheelbase': [self.wheelbase],
+                'carlength': [self.carlength], 
+                'carwidth' : [self.carwidth], 
+                'carheight': [self.carheight], 
+                'curbweight': [self.curbweight], 
+                'enginetype': [self.enginetype],
+                'cylindernumber': [self.cylindernumber],
+                'enginesize' : [self.enginesize], 
+                'fuelsystem': [self.fuelsystem], 
+                'boreratio' : [self.boreratio], 
+                'stroke' : [self.stroke],
+                'compressionratio' : [self.compressionratio], 
+                'horsepower' : [self.horsepower], 
+                'peakrpm' : [self.peakrpm], 
+                'citympg' : [self.citympg], 
+                'highwaympg' : [self.highwaympg]
+            }
+
+            return pd.DataFrame(custom_data_input_dict)
+
         except Exception as e:
             raise CustomException(e, sys)
